b45e1dc1ba81070a7dc41f1d924df61e9991676d
Franck Nijhof <frenck@addons.community>
2019-08-21 20:30:18 +0200

    :hammer: Use Hass.io DNS as NGinx resolver

--- ide/rootfs/etc/cont-init.d/nginx.sh
+++ ide/rootfs/etc/cont-init.d/nginx.sh
@@ -3,6 +3,8 @@
 # Community Hass.io Add-ons: IDE
 # Configure the use of SSL in NGINX
 # ==============================================================================
+declare hassio_dns
+
 bashio::config.require.ssl
 if bashio::config.true 'ssl'; then
     certfile=$(bashio::config 'certfile')
@@ -11,3 +13,7 @@ if bashio::config.true 'ssl'; then
     sed -i "s#%%certfile%%#${certfile}#g" /etc/nginx/nginx-ssl.conf
     sed -i "s#%%keyfile%%#${keyfile}#g" /etc/nginx/nginx-ssl.conf
 fi
+
+hassio_dns=$(bashio::dns.host)
+sed -i "s/%%hassio_dns%%/${hassio_dns}/g" /etc/nginx/nginx-ssl.conf
+sed -i "s/%%hassio_dns%%/${hassio_dns}/g" /etc/nginx/nginx.conf
